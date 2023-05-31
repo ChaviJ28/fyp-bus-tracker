2a0072d820e940a0e4003f163a1c626908db8a14
ChaviJ28 <surujbhalichavi@gmail.com>
2023-06-01 02:30:40 +0400

    environment changes + localStorage

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: "http://192.168.100.56:3002/api"
+};
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: "http://102.222.106.112:3002/api"
+};
