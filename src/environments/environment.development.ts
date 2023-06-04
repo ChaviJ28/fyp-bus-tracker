cf918ca24563067d3d81724b242b666895d2235e
ChaviJ28 <surujbhalichavi@gmail.com>
2023-06-04 21:41:12 +0400

    env url www

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: "http://localhost:3002/api"
+    apiUrl: "http://localhost:3000/api"
     // apiUrl: "https://bus-tracker.azurewebsites.net/api"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: "https://lameouver.org/api"
+    apiUrl: "https://www.lameouver.org/api"
     
 };
