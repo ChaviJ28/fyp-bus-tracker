10f033e793170f0a182ce79125300e5bd533bc63
ChaviJ28 <surujbhalichavi@gmail.com>
2023-06-04 18:37:58 +0400

    proxt test

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: "https://bus-tracker.azurewebsites.net/api" 
+    apiUrl: "http://102.222.106.112:3002/api" 
+    // apiUrl: "https://bus-tracker.azurewebsites.net/api" 
 };
