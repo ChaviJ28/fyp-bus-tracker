ff697858264de0d142419297887548db53afc4fc
ChaviJ28 <surujbhalichavi@gmail.com>
2023-07-12 19:12:56 +0400

    https + www

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: "www.lameouver.org/api" 
+    apiUrl: "https://www.lameouver.org/api" 
     // apiUrl: "https://bus-tracker.azurewebsites.net/api" 
 };
